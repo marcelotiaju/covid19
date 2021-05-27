8e948e608727f606fc0781b2df3e30f5888d2230
marcelotiaju <marcelotiaju@gmail.com>
2021-05-27 16:37:29 -0300

    Primeiro Commit

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -1,13 +1,13 @@
 import React, { memo } from 'react'
-import RefreshIcon from '../../../assets/images/refresh.svg'
-import { Card, Typography, Button, Select, MenuItem } from '../../../components'
+// import RefreshIcon from '../../../assets/images/refresh.svg'
+import { Card, Typography, Select, MenuItem } from '../../../components'
 import COUNTRIES from '../../../commons/constants/countries'
 import { CardPanelContentStyled, ItemStyled } from './style'
 
-const navigatorHasShare = navigator.share
+// const navigatorHasShare = navigator.share
 
-function Panel({ updateAt, onChange, data, country, getCoviddata }) {
-  const { cases, recovered, deaths, todayCases, todayDeaths } = data
+function Panel({ updateAt, onChange, data, country }) {
+  // const {  recovered } = data
 
   const renderCountries = (country, index) => (
     <MenuItem key={`country-${index}`} value={country.value}>
@@ -18,42 +18,34 @@ function Panel({ updateAt, onChange, data, country, getCoviddata }) {
     </MenuItem>
   )
 
-  const textCovid19 = `País: ${country} - recuperados: ${recovered}`
+  // const textCovid19 = `País: ${country} - recuperados: ${recovered}`
 
-  const copyInfo = () => {
-    navigator.clipboard.writeText(textCovid19)
-  }
+  // const copyInfo = () => {
+  //   navigator.clipboard.writeText(textCovid19)
+  // }
 
-  const shareInfo = () => {
-    navigator.share({
-      title: `Dados do Covid19 - ${country}`,
-      text: textCovid19,
-      url: 'https://covid19dio.netlify.app/'
-    })
-  }
+  // const renderShareButton = (
+  //   <div>
+  //     <Button variant="contained" color="primary" onClick={shareInfo}>
+  //       Compartilhar
+  //     </Button>
+  //   </div>
+  // )
 
-  const renderShareButton = (
-    <div>
-      <Button variant="contained" color="primary" onClick={shareInfo}>
-        Compartilhar
-      </Button>
-    </div>
-  )
-
-  const renderCopyButton = (
-    <div>
-      <Button variant="contained" color="primary" onClick={copyInfo}>
-        Copiar
-      </Button>
-    </div>
-  )
+  // const renderCopyButton = (
+  //   <div>
+  //     <Button variant="contained" color="primary" onClick={copyInfo}>
+  //       Copiar
+  //     </Button>
+  //   </div>
+  // )
 
   return (
     <Card>
       <CardPanelContentStyled>
         <div>
-          <Typography variant="h5" component="span" color="primary">COVID19</Typography>
-          <Typography variant="h6" component="span" color="primary">Painel Coronavírus</Typography>
+          <Typography variant="h5" component="span" color="primary">COVID19 </Typography>
+          <Typography variant="h6" component="span" color="primary">Painel Coronavírus </Typography>
           <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
           <div className="pt-2">
             <Select onChange={onChange} value={country}>
